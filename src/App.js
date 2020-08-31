5424b144b9d7854d1d768a4f933423d955a5622b
azm98 <49077908+azm98@users.noreply.github.com>
2020-08-31 12:32:34 -0700

    intiaialize material ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './navbar';
+import { makeStyles } from '@material-ui/core/styles';
+import {Paper,TextField,Button } from '@material-ui/core';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    
+  
+    marginTop:10
+
+   
+    
+  },
+  paper:{
+   
+    width:400,
+    margin:'0 auto',
+    textAlign:'center',
+    padding:'15 0'
+  
+  },
+  input:{
+    width:'95%',
+    marginBottom:15
+  }
+}));
+
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+            <NavBar/>
+      <Paper elevation={3} className={classes.paper} >
+
+      <h1>Login</h1>
+      <form className={classes.root} noValidate autoComplete="off">
+      <TextField  label="Enter Email" className={classes.input} />
+      <TextField  label="Enter Password" className={classes.input} />
+     
+    </form>
+    <Button variant="contained" color="primary" className={classes.input}>
+        Login
+      </Button>
+    </Paper>
     </div>
   );
 }
